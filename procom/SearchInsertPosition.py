87e77d753d2f06edead1807be9f5ad39b10f88a3
matias ignacio erenchun marquez <matiaserenchun@gmail.com>
2022-05-08 15:01:12 -0400

    searchin listo

--- procom/SearchInsertPosition.py
+++ procom/SearchInsertPosition.py
@@ -1,33 +1,34 @@
 
 ##wip()
-nums = [1,3,5,6,7,8,9,10,11,33,44,66,77]
-target = 5
+nums = [1]
+target = 1
 #olvide como implementar esta wea XDXDXD
 inferior=0
 superior = len(nums)-1
 index = int(superior/2)
-i=0
 continuar = True
 while continuar:
+    print("inferior: ", inferior, " superior:", superior, " index:", index)
     if(nums[index]==target):
-        continuar = False
-    elif(nums[index]>=target):
-        if (nums[index] == target):
-            print("dawn", index)
-            continuar = False
-        else:
-            superior = index
-            index = int((inferior + superior)/2)
-    elif(nums[index]<=target):
-        if (nums[index] == target):
-            print("upp", index)
-            continuar = False
-        else:
-            inferior = index
-            index = int((inferior + superior) / 2)
-    print("inferior: ", inferior,"superior: ",superior)
-    if(i==4):
         continuar=False
-    i+=1
-
+    elif(nums[inferior]==target):
+        index = inferior
+        continuar=False
+    elif(nums[superior]==target):
+        index = superior
+        continuar = False
+    elif(nums[index]>target):
+        superior=index
+        index=int((inferior+superior)/2)
+    elif(nums[index]<target):
+        inferior = index
+        index = int((inferior + superior) / 2)
+    if(index==inferior or index==superior):
+        continuar=False
+        if(nums[inferior]<target and nums[superior]>target):
+            index=superior
+        elif(nums[inferior]>target):
+            index=inferior
+        elif(nums[superior]<target):
+            index=superior+1
 print(index)
